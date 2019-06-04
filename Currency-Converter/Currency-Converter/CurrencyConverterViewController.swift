7f1e11f03a2fb1b9773ed496aad77adcbc74d2cc
Marlon Raskin <marlonjraskin@gmail.com>
2019-06-03 21:39:51 -0400

    Rebuilding app. Added all layouts and constraints, added convert func and applied it to convertButtonPressed, created enum, created all necessary outlets and actions applied some styling.

---
+++ Currency-Converter/Currency-Converter/CurrencyConverterViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  Currency-Converter
+//
+//  Created by Marlon Raskin on 6/3/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyConverterViewController: UIViewController {
+
+	var currencyType: CurrencyType = .cad
+	
+	
+	@IBOutlet var toCurrencyTextField: UITextField!
+	@IBOutlet var fromCurrencyTextField: UITextField!
+	@IBOutlet var toCurrencyLabel: UILabel!
+	@IBOutlet var cadButton: UIButton!
+	@IBOutlet var mxnButton: UIButton!
+	@IBOutlet var convertButton: UIButton!
+	@IBAction func convertButtonPressed(_ sender: UIButton) {
+		guard let userInput = fromCurrencyTextField.text else {
+			fromCurrencyTextField.text = "Invalid amount entered"
+			return
+		}
+		toCurrencyTextField.text = currencyFormatter.string(for: convert(dollars: Double(userInput) ?? 0.0, to: currencyType))
+	}
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = UIColor(red: 0.56, green: 0.89, blue: 0.83, alpha: 1)
+		convertButton.backgroundColor = UIColor(red: 0.40, green: 0.22, blue: 0.94, alpha: 1)
+		convertButton.tintColor = UIColor.white
+		convertButton.layer.cornerRadius = 18
+		cadButton.tintColor = UIColor(red: 0.40, green: 0.22, blue: 0.94, alpha: 1)
+		cadButton.isSelected = true
+		
+	}
+
+	
+	enum CurrencyType {
+		case cad
+		case mxn
+	}
+	
+	var currencyFormatter: NumberFormatter = {
+		let formatter = NumberFormatter()
+		formatter.numberStyle = .currency
+		return formatter
+	}()
+	
+	func convert(dollars: Double, to unit: CurrencyType) -> Double {
+		let cad = dollars * 1.34
+		let mxn = dollars * 19.77
+		var outcome: Double
+		
+		switch unit {
+		case .cad:
+			outcome = cad
+		case .mxn:
+			outcome = mxn
+		}
+		return outcome
+	}
+
+}
+
